60a34f53e9e8e53b19a8149bace81fe77dccc96a
eleves <eleves@MP-0124-120007.local>
2016-11-29 15:57:41 +0100

    create to app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+var http = require('http'),
+	fs = require('fs');
+	
+var server = http.create(function(req, res){
+	fs.readFile('public/home-4.html', function(err, html) {
+		if(err) throw err;
+		else {
+			res.writeHeader(200, {"Content-Type": "text/html"});
+			res.write(html);
+			res.end();
+		} 
+	}
+});
+
+server.listen(1337);
